d849857a1d2958aa38cab85c7f20a9b03a8ee560
Robert Snyder <r.snyder49@gmail.com>
2018-08-23 22:11:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,5 +52,13 @@ class Song
   
   def self.artist_count 
     artist_hash = {}
-    
+    @@artists.each do |artist|
+      if artist_hash[artist] == nil 
+        artist_hash[artist] = 1 
+      else 
+        artist_hash[artist] += 1 
+      end 
+    end 
+    artist_hash
+  end 
 end 
